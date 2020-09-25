e404f74309ddce714e5ecd66b3ea136bace28252
Brad Treloar <brad@allbizsupplies.biz>
2020-09-25 13:30:43 +0930

    Add isAdmin indicator to fixture

--- src/fixtures/IsAuthenticated.tsx
+++ src/fixtures/IsAuthenticated.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const IsAuthenticatedFixture: React.FC<{
   isAuthenticated: boolean;
-}> = ({ isAuthenticated }) => {
-  return isAuthenticated ? (
-    <div>User is logged in.</div>
-  ) : (
-    <div>User is not logged in.</div>
+  isAdmin?: boolean;
+}> = ({ isAuthenticated, isAdmin }) => {
+  return (
+    <>
+      {isAuthenticated ? (
+        <div>User is logged in.</div>
+      ) : (
+        <div>User is not logged in.</div>
+      )}
+      {isAuthenticated && isAdmin && <div>User is admin.</div>}
+    </>
   );
 };
 
