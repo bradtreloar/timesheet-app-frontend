1c39105418d2b5b87da0b5459481f573812fd2cb
Brad Treloar <brad@allbizsupplies.biz>
2020-10-07 11:45:43 +1030

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import React from "react";
-import "./App.css";
-import { useAuth } from "./context/auth";
-import LoginPage from "./pages/LoginPage";
 
-function App() {
-  const { isAuthenticated, login, logout } = useAuth();
-
-  return isAuthenticated ? (
-    <div>
-      <p>You are signed in.</p>
-      <a
-        onClick={() => {
-          logout();
-        }}
-      >
-        Log out
-      </a>
-    </div>
-  ) : (
-    <div>
-      <LoginPage />
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return null;
+};
 
 export default App;
