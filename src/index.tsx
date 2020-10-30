d37a90f80b1c6b6e3aff9dc9b6a470122c9be9d8
Brad Treloar <brad@allbizsupplies.biz>
2020-10-30 11:28:40 +1030

    add BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { AuthProvider } from "./context/auth";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
