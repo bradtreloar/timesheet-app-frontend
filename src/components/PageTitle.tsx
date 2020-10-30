8dbdf620c5dcf2a3bd5dfe725dd76fc63b4925c4
Brad Treloar <brad@allbizsupplies.biz>
2020-10-30 11:50:27 +1030

    restyle page title

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const PageTitle: React.FC = ({ children }) => (
-  <div className="page-title bg-white border-bottom mb-3">
-    <div className="py-5 text-center">
+  <div className="container mb-3">
+    <div className="py-3">
       <h1>{children}</h1>
     </div>
   </div>
