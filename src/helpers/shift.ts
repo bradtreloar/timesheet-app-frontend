4d56f40195189f863a4e2378011b333f4c5408a7
Brad Treloar <brad@allbizsupplies.biz>
2020-11-18 11:56:32 +1030

    add getShiftFromTimes helper

--- src/helpers/shift.ts
+++ src/helpers/shift.ts
@@ -1,4 +1,4 @@
-import { ShiftTimes } from "../types";
+import { Shift, ShiftTimes } from "../types";
 import { SimpleTime } from "./date";
 
 export const getShiftDuration = (shiftTimes: ShiftTimes) => {
@@ -15,4 +15,28 @@ export const getShiftDuration = (shiftTimes: ShiftTimes) => {
   }
 
   return null;
+};
+
+export const getShiftFromTimes = (date: Date, shiftTimes: ShiftTimes): Shift => {
+  if (shiftTimes === null) {
+    throw new Error(`No shift times.`);
+  }
+
+  const { startTime, endTime, breakDuration } = shiftTimes;
+  if (!(startTime && endTime && breakDuration)) {
+    throw new Error(`Invalid shift times.`);
+  }
+
+  const shiftDuration =
+    endTime.toMinutes() - startTime.toMinutes() - breakDuration.toMinutes();
+
+  if (shiftDuration <= 0) {
+    throw new Error(`Invalid shift times.`);
+  }
+
+  return {
+    start: startTime.toDate(date),
+    end: endTime.toDate(date),
+    breakDuration: breakDuration.toMinutes(),
+  };
 };
