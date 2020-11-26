3958fde1f027603158b81a1cd96b38b49a590535
Brad Treloar <brad@allbizsupplies.biz>
2020-11-26 13:06:51 +1030

    add UserSettingsForm

---
+++ src/components/UserSettingsForm.test.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import UserSettingsForm from "./UserSettingsForm";
+import { randomPassword, randomUser } from "../fixtures/random";
+import { noop } from "lodash";
+import { User } from "../types";
+
+const testUser = randomUser();
+const { name: testName, email: testEmail } = randomUser();
+
+test("Form renders", () => {
+  render(<UserSettingsForm user={testUser} onSubmit={noop} />);
+});
+
+test("Form submission succeeds", (done) => {
+  render(
+    <UserSettingsForm
+      user={testUser}
+      onSubmit={(user) => {
+        expect(user.email).toEqual(testEmail);
+        expect(user.name).toEqual(testName);
+        done();
+      }}
+    />
+  );
+
+  userEvent.clear(screen.getByLabelText(/your name/i));
+  userEvent.type(screen.getByLabelText(/your name/i), testName);
+  userEvent.clear(screen.getByLabelText(/your email address/i));
+  userEvent.type(screen.getByLabelText(/your email address/i), testEmail);
+  userEvent.click(screen.getByText(/save/i));
+});
+
+test("Empty form submission fails", () => {
+  render(
+    <UserSettingsForm
+      user={testUser}
+      onSubmit={() => {
+        throw new Error("onSubmit should not be called.");
+      }}
+    />
+  );
+
+  userEvent.clear(screen.getByLabelText(/your email address/i));
+  userEvent.clear(screen.getByLabelText(/your name/i));
+  userEvent.click(screen.getByText(/save/i));
+  expect(screen.getAllByText(/required/i)).toHaveLength(2);
+});
+
+test("Reject invalid form input", () => {
+  const invalidEmail = testEmail.replace("@", "_");
+
+  render(
+    <UserSettingsForm
+      user={testUser}
+      onSubmit={() => {
+        throw new Error("onSubmit should not be called.");
+      }}
+    />
+  );
+
+  userEvent.clear(screen.getByLabelText(/your email address/i));
+  userEvent.type(screen.getByLabelText(/your email address/i), invalidEmail);
+  userEvent.click(screen.getByText(/save/i));
+  screen.getByText(/must be a valid email address/i);
+});
+
+test("Form handles pending authentication", () => {
+  render(
+    <UserSettingsForm
+      user={testUser}
+      onSubmit={() => {
+        throw new Error("onSubmit should not be called.");
+      }}
+      pending
+    />
+  );
+
+  userEvent.click(screen.getByText(/saving/i));
+});
---
+++ src/components/UserSettingsForm.tsx
@@ -0,0 +1,103 @@
+import React, { useMemo } from "react";
+import * as EmailValidator from "email-validator";
+import Form from "react-bootstrap/Form";
+import { useForm } from "../form/form";
+import { Alert, Button } from "react-bootstrap";
+import { User } from "../types";
+
+interface UserSettingsFormValues {
+  name: string;
+  email: string;
+}
+
+const validate = (values: UserSettingsFormValues) => {
+  const errors = {} as { [key: string]: any };
+  const { name, email } = values;
+
+  if (name === "") {
+    errors.name = `Required`;
+  }
+
+  if (email === "") {
+    errors.email = `Required`;
+  } else if (EmailValidator.validate(email) === false) {
+    errors.email = `Must be a valid email address`;
+  }
+
+  return errors;
+};
+
+const process = (values: any, user: User): User =>
+  Object.assign({}, user, {
+    name: values.name,
+    email: values.email,
+  });
+
+interface UserSettingsFormProps {
+  user: User;
+  onSubmit: (user: User) => void;
+  pending?: boolean;
+}
+
+const UserSettingsForm: React.FC<UserSettingsFormProps> = ({
+  user,
+  onSubmit,
+  pending,
+}) => {
+  const initialValues = {
+    name: user.name,
+    email: user.email,
+  };
+
+  const {
+    values,
+    errors,
+    visibleErrors,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+  } = useForm(
+    initialValues,
+    (values) => onSubmit(process(values, user)),
+    validate
+  );
+
+  return (
+    <Form className="form-narrow" onSubmit={handleSubmit}>
+      {errors.form && <Alert variant="danger">{errors.form}</Alert>}
+      <Form.Group controlId="name">
+        <Form.Label>Your name</Form.Label>
+        <Form.Control
+          type="text"
+          name="name"
+          isInvalid={visibleErrors.name}
+          value={values.name}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+        {visibleErrors.name && (
+          <Form.Control.Feedback>{errors.name}</Form.Control.Feedback>
+        )}
+      </Form.Group>
+      <Form.Group controlId="email">
+        <Form.Label>Your email address</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          isInvalid={visibleErrors.email}
+          value={values.email}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+        {visibleErrors.email && (
+          <Form.Control.Feedback>{errors.email}</Form.Control.Feedback>
+        )}
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={pending}>
+        {pending ? `Saving` : `Save password`}
+      </Button>
+    </Form>
+  );
+};
+
+export default UserSettingsForm;
