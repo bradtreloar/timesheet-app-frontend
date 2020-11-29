082c76d2ee0337922ba6aa87165da187472e8d42
Brad Treloar <brad@treloardigital.com.au>
2020-11-29 17:48:06 +1030

    remove redundant middleware assignment

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,10 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import timesheets from "./timesheets";
-
-const middleware = [...getDefaultMiddleware()];
 
 const store = configureStore({
   reducer: {
     timesheets,
   },
-  middleware,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
