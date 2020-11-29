fc71f1a73e2d1a6478b8b8c8c03863656bb2ce78
Brad Treloar <brad@treloardigital.com.au>
2020-11-29 16:57:57 +1030

    add tests for NotFoundPage

---
+++ src/pages/NotFoundPage.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { act, render, screen } from "@testing-library/react";
+import { AuthProvider } from "../context/auth";
+import NotFoundPage from "./NotFoundPage";
+import * as datastore from "../services/datastore";
+import { MemoryRouter } from "react-router";
+
+jest.mock("../services/datastore");
+jest.spyOn(datastore, "fetchCurrentUser").mockResolvedValue(null);
+
+const Fixture: React.FC = () => {
+  return (
+    <AuthProvider>
+      <MemoryRouter>
+        <NotFoundPage />
+      </MemoryRouter>
+    </AuthProvider>
+  );
+};
+
+test("renders 404 page", async () => {
+  await act(async () => {
+    render(<Fixture />);
+  });
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/404/i);
+});
