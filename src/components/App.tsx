f5cfb4cb9003c1d808547597d27eef57e2f1a9d7
Brad Treloar <brad@treloardigital.com.au>
2020-12-01 21:39:50 +1030

    add routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,18 @@ import { Route, Switch } from "react-router-dom";
 import GuestRoute from "components/routes/GuestRoute";
 import ProtectedRoute from "components/routes/ProtectedRoute";
 import { useAuth } from "context/auth";
-import HomePage from "components/pages/HomePage";
 import LoginPage from "components/pages/LoginPage";
 import NotFoundPage from "components/pages/NotFoundPage";
 import store from "store";
 import { fetchTimesheets } from "store/timesheets";
 import { User } from "types";
+import PasswordResetPage from "./pages/PasswordResetPage";
+import PasswordPage from "./pages/PasswordPage";
+import TimesheetConfirmPage from "./pages/TimesheetConfirmPage";
+import TimesheetPage from "./pages/TimesheetPage";
+import AdminRoute from "./routes/AdminRoute";
+import SettingsPage from "./pages/SettingsPage";
+import HomePage from "./pages/HomePage";
 
 const initialiseStore = async (user: User) => {
   store.dispatch(fetchTimesheets(user));
@@ -32,8 +38,23 @@ const App: React.FC = () => {
       <ProtectedRoute exact path="/">
         <HomePage />
       </ProtectedRoute>
+      <ProtectedRoute exact path="/timesheet/new">
+        <TimesheetPage />
+      </ProtectedRoute>
+      <ProtectedRoute exact path="/timesheet/confirmation">
+        <TimesheetConfirmPage />
+      </ProtectedRoute>
+      <ProtectedRoute exact path="/account/new-password">
+        <PasswordPage />
+      </ProtectedRoute>
+      <AdminRoute exact path="/settings">
+        <SettingsPage />
+      </AdminRoute>
       <GuestRoute exact path="/login">
         <LoginPage />
+      </GuestRoute>
+      <GuestRoute exact path="/reset-password">
+        <PasswordResetPage />
       </GuestRoute>
       <Route>
         <NotFoundPage />
