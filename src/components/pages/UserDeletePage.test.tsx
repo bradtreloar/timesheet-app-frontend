602c90bc22d01257895ad7e5135659d1deb4406c
Brad Treloar <brad@treloardigital.com.au>
2020-12-06 01:41:09 +1030

    add UserDeletePage

---
+++ src/components/pages/UserDeletePage.test.tsx
@@ -0,0 +1,73 @@
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { AuthProvider } from "context/auth";
+import { MessagesProvider } from "context/messages";
+import {
+  randomSettings,
+  randomTimesheets,
+  randomUser,
+  randomUsers,
+} from "fixtures/random";
+import React from "react";
+import { Provider } from "react-redux";
+import { MemoryRouter, Route } from "react-router";
+import faker from "faker";
+import * as datastore from "services/datastore";
+import store from "store";
+import { clearUsers, setUsers } from "store/users";
+import UserFormPage from "./UserFormPage";
+import UserDeletePage from "./UserDeletePage";
+
+jest.mock("services/datastore");
+const testUser = randomUser();
+
+const Fixture: React.FC<{
+  initialEntries?: string[];
+}> = ({ children, initialEntries }) => {
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <MessagesProvider>
+          <MemoryRouter initialEntries={initialEntries}>
+            <Route path="/user/:id/delete">
+              <UserDeletePage />
+            </Route>
+          </MemoryRouter>
+        </MessagesProvider>
+      </AuthProvider>
+    </Provider>
+  );
+};
+
+beforeAll(() => {
+  store.dispatch(clearUsers);
+  jest.spyOn(datastore, "fetchCurrentUser").mockResolvedValue(testUser);
+  localStorage.setItem("user", JSON.stringify(testUser));
+  jest.spyOn(datastore, "deleteUser").mockResolvedValue(testUser);
+  store.dispatch(setUsers([testUser]));
+});
+
+test("renders", async () => {
+  await act(async () => {
+    render(<Fixture initialEntries={[`/user/${testUser.id}/delete`]} />);
+  });
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/delete user/i);
+});
+
+test("handles clicking delete button", async () => {
+  await act(async () => {
+    render(<Fixture initialEntries={[`/user/${testUser.id}/delete`]} />);
+  });
+
+  await act(async () => {
+    screen.getAllByText(/delete user/i).forEach(element => {
+      if (element.getAttribute("type") === "button") {
+        userEvent.click(element);
+      }
+    });
+  });
+
+  const { users } = store.getState().users;
+  expect(users).toStrictEqual([]);
+});
