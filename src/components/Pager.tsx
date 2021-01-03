d114c72af3641d3e53766040f39c1b305fdc1b7d
Brad Treloar <brad@treloardigital.com.au>
2021-01-04 04:10:17 +1030

    add pagination

---
+++ src/components/Pager.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Pagination } from "react-bootstrap";
+
+interface PagerProps {
+  pageIndex: number;
+  pageCount: number;
+  onChange: (pageIndex: number) => void;
+}
+
+interface PagerButtonProps {
+  label: string;
+  srLabel: string;
+  disabled?: boolean;
+  index: number;
+}
+
+const Pager: React.FC<PagerProps> = ({ pageIndex, pageCount, onChange }) => {
+  const PagerButton: React.FC<PagerButtonProps> = ({
+    label,
+    srLabel,
+    index,
+    disabled,
+  }) => (
+    <li className="page-item">
+      <button
+        disabled={disabled || index === pageIndex}
+        className="page-link"
+        type="button"
+        onClick={(event) => {
+          event.preventDefault();
+          onChange(index);
+        }}
+      >
+        <span>{label}</span>
+        <span className="sr-only">{srLabel}</span>
+      </button>
+    </li>
+  );
+
+  const lastPageIndex = pageCount - 1;
+
+  return (
+    <Pagination className="justify-content-center">
+      <>
+        <PagerButton index={0} label="«" srLabel="First" />
+        <PagerButton index={pageIndex - 1} label="‹" srLabel="Previous" />
+      </>
+      <PagerButton index={0} label={`Page ${pageIndex + 1} of ${pageCount}`} srLabel="(current)" />
+      <>
+        <PagerButton index={pageIndex + 1} label="›" srLabel="Next" />
+        <PagerButton index={lastPageIndex} label="»" srLabel="Last" />
+      </>
+    </Pagination>
+  );
+};
+
+export default Pager;
--- src/components/tables/TimesheetTable.tsx
+++ src/components/tables/TimesheetTable.tsx
@@ -1,18 +1,33 @@
+import React, { useMemo, useState } from "react";
 import { DateTime } from "luxon";
-import React from "react";
 import { Link } from "react-router-dom";
-import {
-  getTimesheetTotalHours,
-} from "services/date";
+import { getTimesheetTotalHours } from "services/date";
 import { Timesheet } from "types";
+import Pager from "components/Pager";
+
+const ITEMS_PER_PAGE = 10;
 
 interface TimesheetTableProps {
   timesheets: Timesheet[];
 }
 
 const TimesheetTable: React.FC<TimesheetTableProps> = ({ timesheets }) => {
-  const rows = timesheets.map((timesheet, index) => {
-    const created = DateTime.fromISO(timesheet.created as string).toLocaleString(DateTime.DATE_SHORT);
+  const [pageIndex, setPageIndex] = useState(0);
+
+  const pageCount = useMemo(
+    () =>
+      timesheets.length > 0 ? Math.ceil(timesheets.length / ITEMS_PER_PAGE) : 0,
+    [timesheets]
+  );
+
+  const firstItem = pageIndex * ITEMS_PER_PAGE;
+  const lastItem = firstItem + ITEMS_PER_PAGE;
+  const visibleTimesheets = timesheets.slice(firstItem, lastItem);
+
+  const tableRows = visibleTimesheets.map((timesheet, index) => {
+    const created = DateTime.fromISO(
+      timesheet.created as string
+    ).toLocaleString(DateTime.DATE_SHORT);
     const totalHours = getTimesheetTotalHours(timesheet);
     const pathname = `/timesheet/${timesheet.id}`;
 
@@ -27,15 +42,24 @@ const TimesheetTable: React.FC<TimesheetTableProps> = ({ timesheets }) => {
   });
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Date submitted</th>
-          <th className="text-right">Total hours</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Date submitted</th>
+            <th className="text-right">Total hours</th>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </table>
+      <div>
+        <Pager
+          pageIndex={pageIndex}
+          pageCount={pageCount}
+          onChange={setPageIndex}
+        />
+      </div>
+    </>
   );
 };
 
