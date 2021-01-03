77034b46a131ebe37781df2c5af7a43a038b16c3
Brad Treloar <brad@treloardigital.com.au>
2021-01-04 05:38:31 +1030

    refactor sort logic and add pager

--- src/components/tables/UserTable.tsx
+++ src/components/tables/UserTable.tsx
@@ -1,3 +1,4 @@
+import usePagination from "hooks/usePagination";
 import { orderBy } from "lodash";
 import React, { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
@@ -9,25 +10,34 @@ interface UserTableProps {
 
 interface TableHeadingProps {
   name: keyof User;
-  sortOrder?: boolean;
   className?: string;
 }
 
-const UserTable: React.FC<UserTableProps> = ({ users }) => {
-  const [sortOrder, setSortOrder] = useState({
-    column: "name" as keyof User,
-    ascending: true,
-  });
+interface SortOrder {
+  column: keyof User;
+  ascending: boolean;
+}
 
-  const sortedUsers = useMemo(
-    () =>
-      orderBy(
-        users,
-        [sortOrder.column],
-        [sortOrder.ascending ? "asc" : "desc"]
-      ),
-    [users, sortOrder]
+const defaultSortOrder: SortOrder = {
+  column: "name" as keyof User,
+  ascending: true,
+};
+
+const sortedItems = function <T>(items: T[], sortOrder: SortOrder) {
+  return orderBy(
+    items,
+    [sortOrder.column],
+    [sortOrder.ascending ? "asc" : "desc"]
   );
+};
+
+const UserTable: React.FC<UserTableProps> = ({ users }) => {
+  const [sortOrder, setSortOrder] = useState(defaultSortOrder);
+  const sortedUsers = useMemo(() => sortedItems(users, sortOrder), [
+    users,
+    sortOrder,
+  ]);
+  const { items, Pager } = usePagination(sortedUsers);
 
   const handleClickHeading = (column: keyof User) => {
     if (sortOrder.column === column) {
@@ -45,46 +55,52 @@ const UserTable: React.FC<UserTableProps> = ({ users }) => {
 
   const TableHeading: React.FC<TableHeadingProps> = ({
     name,
-    sortOrder,
     className,
     children,
   }) => (
     <th className={className} onClick={() => handleClickHeading(name)}>
       {children}
-      {sortOrder !== undefined && (
-        <span className="sort-icon">{sortOrder ? `▼` : `▲`}</span>
+      {sortOrder.column === name && (
+        <span className="sort-icon ml-2">
+          {sortOrder.ascending ? `▲` : `▼`}
+        </span>
       )}
     </th>
   );
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <TableHeading name={"name"}>
-            <span>Name</span>
-          </TableHeading>
-          <TableHeading name={"email"}>
-            <span>Email</span>
-          </TableHeading>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedUsers.map((user, index) => {
-          const userPageUrl = `/users/${user.id}`;
-          return (
-            <tr key={index} data-testid="user-index-table-row">
-              <td>
-                <Link to={userPageUrl}>{user.name}</Link>
-              </td>
-              <td>
-                <Link to={userPageUrl}>{user.email}</Link>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <TableHeading name={"name"}>
+              <span>Name</span>
+            </TableHeading>
+            <TableHeading name={"email"}>
+              <span>Email</span>
+            </TableHeading>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((user, index) => {
+            const userPageUrl = `/users/${user.id}`;
+            return (
+              <tr key={index} data-testid="user-index-table-row">
+                <td>
+                  <Link to={userPageUrl}>{user.name}</Link>
+                </td>
+                <td>
+                  <Link to={userPageUrl}>{user.email}</Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <Pager />
+      </div>
+    </>
   );
 };
 
