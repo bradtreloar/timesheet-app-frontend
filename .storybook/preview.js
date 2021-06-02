177d7c4a2c08fd65a864b86d859bbb5de72e5cc2
Brad Treloar <brad.treloar@pm.me>
2021-06-02 09:56:44 +0930

    reinstall storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,10 @@ import "../src/styles/common/bootstrap.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-};
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
