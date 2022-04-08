963ba1ae384db29def8e2c3c2d16c45b3b03be49
Brad Treloar <brad.treloar@pm.me>
2022-04-08 11:45:14 +0930

    remove redundant null checks

--- src/timesheets/store/index.ts
+++ src/timesheets/store/index.ts
@@ -8,18 +8,14 @@ import { AppAsyncThunkAction, AppThunkAction } from "./types";
 export const fetchTimesheetEntries = (
   timesheet: Timesheet
 ): AppAsyncThunkAction => async (dispatch, getState) => {
-  if (shiftActions.fetchAllBelongingTo !== null) {
-    await shiftActions.fetchAllBelongingTo(timesheet.id)(
-      dispatch,
-      getState,
-      null
-    );
-  }
-  if (absenceActions.fetchAllBelongingTo !== null) {
-    await absenceActions.fetchAllBelongingTo(timesheet.id)(
-      dispatch,
-      getState,
-      null
-    );
-  }
+  await shiftActions.fetchAllBelongingTo(timesheet.id)(
+    dispatch,
+    getState,
+    null
+  );
+  await absenceActions.fetchAllBelongingTo(timesheet.id)(
+    dispatch,
+    getState,
+    null
+  );
 };
