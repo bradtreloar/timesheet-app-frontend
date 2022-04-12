edf817b2f5f9a5d4845f5d3618ff55b79e9c7f58
Brad Treloar <brad.treloar@pm.me>
2022-04-12 12:06:59 +0930

    Fetch timesheet and entries if not already in store.
    TimesheetViewPage will dispatch fetchActions for the timesheet and
    its shifts and absences if they are not found in the store. Page does
    not display a NotFoundPage until it the fetch attempt fails.

--- src/timesheets/pages/TimesheetViewPage.test.tsx
+++ src/timesheets/pages/TimesheetViewPage.test.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { act, render, screen } from "@testing-library/react";
 import { MemoryRouter, Route } from "react-router-dom";
-import { randomCurrentUser, randomTimesheet } from "fixtures/random";
+import {
+  randomAbsence,
+  randomCurrentUser,
+  randomID,
+  randomShift,
+  randomTimesheet,
+} from "fixtures/random";
 import TimesheetViewPage from "./TimesheetViewPage";
 import { Provider } from "react-redux";
 import { actions as timesheetActions } from "timesheets/store/timesheets";
+import { actions as shiftActions } from "timesheets/store/shifts";
+import { actions as absenceActions } from "timesheets/store/absences";
 import createStore, { AppStore } from "store/createStore";
 import { buildEntityState } from "store/entity";
 import { mockAuthContext, MockAuthProvider } from "fixtures/auth";
 import { MessagesProvider } from "messages/context";
 import { AuthContextValue } from "auth/context";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { DateTime } from "luxon";
+import {
+  EntityNotFoundException,
+  UnauthorizedEntityRequestException,
+} from "datastore/entity";
 
 const Fixture: React.FC<{
   authContextValue: Partial<AuthContextValue>;
@@ -31,7 +45,7 @@ const Fixture: React.FC<{
   );
 };
 
-test("renders timesheet view page", async () => {
+it("renders timesheet view page", async () => {
   const user = randomCurrentUser();
   const timesheet = randomTimesheet(user);
   const store = createStore();
@@ -51,4 +65,157 @@ test("renders timesheet view page", async () => {
   });
 
   expect(screen.getAllByRole("heading")[0]).toHaveTextContent(/timesheet/i);
+});
+
+it("fetches timesheet on mount when not in store", async () => {
+  const user = randomCurrentUser();
+  const timesheet = randomTimesheet(user);
+  const store = createStore();
+  jest
+    .spyOn(timesheetActions, "fetchOne")
+    .mockImplementation(
+      createAsyncThunk("timesheets/fetchOne", () => Promise.resolve(timesheet))
+    );
+
+  await act(async () => {
+    render(
+      <Fixture
+        authContextValue={{
+          isAuthenticated: true,
+          user,
+        }}
+        store={store}
+        initialEntries={[`/timesheet/${timesheet.id}`]}
+      />
+    );
+  });
+
+  expect(timesheetActions.fetchOne).toHaveBeenCalledWith(timesheet.id);
+  expect(store.getState().timesheets.entities).toStrictEqual(
+    buildEntityState([timesheet]).entities
+  );
+});
+
+it("fetches timesheet shifts on mount when not in store", async () => {
+  const user = randomCurrentUser();
+  const timesheet = randomTimesheet(user);
+  const date = DateTime.local();
+  const shift = randomShift(timesheet, date);
+  const store = createStore();
+  timesheet.relationships.shifts = [shift.id];
+  store.dispatch(timesheetActions.set(buildEntityState([timesheet])));
+  jest
+    .spyOn(shiftActions, "fetchAllBelongingTo")
+    .mockImplementation(
+      createAsyncThunk("shifts/fetchAllBelongingTo", () =>
+        Promise.resolve([shift])
+      )
+    );
+
+  await act(async () => {
+    render(
+      <Fixture
+        authContextValue={{
+          isAuthenticated: true,
+          user,
+        }}
+        store={store}
+        initialEntries={[`/timesheet/${timesheet.id}`]}
+      />
+    );
+  });
+
+  expect(shiftActions.fetchAllBelongingTo).toHaveBeenCalledWith(timesheet.id);
+  expect(store.getState().shifts.entities).toStrictEqual(
+    buildEntityState([shift]).entities
+  );
+});
+
+it("fetches timesheet absences on mount when not in store", async () => {
+  const user = randomCurrentUser();
+  const timesheet = randomTimesheet(user);
+  const date = DateTime.local();
+  const absence = randomAbsence(timesheet, date);
+  const store = createStore();
+  timesheet.relationships.absences = [absence.id];
+  store.dispatch(timesheetActions.set(buildEntityState([timesheet])));
+  jest
+    .spyOn(absenceActions, "fetchAllBelongingTo")
+    .mockImplementation(
+      createAsyncThunk("absences/fetchAllBelongingTo", () =>
+        Promise.resolve([absence])
+      )
+    );
+
+  await act(async () => {
+    render(
+      <Fixture
+        authContextValue={{
+          isAuthenticated: true,
+          user,
+        }}
+        store={store}
+        initialEntries={[`/timesheet/${timesheet.id}`]}
+      />
+    );
+  });
+
+  expect(absenceActions.fetchAllBelongingTo).toHaveBeenCalledWith(timesheet.id);
+  expect(store.getState().absences.entities).toStrictEqual(
+    buildEntityState([absence]).entities
+  );
+});
+
+it("renders NotFoundPage when timesheet cannot be found", async () => {
+  const user = randomCurrentUser();
+  const store = createStore();
+  const id = randomID();
+  jest.spyOn(timesheetActions, "fetchOne").mockImplementation(
+    createAsyncThunk("timesheet/fetchOne", () => {
+      throw new EntityNotFoundException("timesheets", id);
+    })
+  );
+
+  await act(async () => {
+    render(
+      <Fixture
+        authContextValue={{
+          isAuthenticated: true,
+          user,
+        }}
+        store={store}
+        initialEntries={[`/timesheet/${id}`]}
+      />
+    );
+  });
+
+  expect(timesheetActions.fetchOne).toHaveBeenCalledWith(id);
+  screen.getByTestId("not-found-message");
+});
+
+it("renders NotFoundPage when timesheet access denied", async () => {
+  const user = randomCurrentUser();
+  const store = createStore();
+  const id = randomID();
+  jest.spyOn(timesheetActions, "fetchOne").mockImplementation(
+    createAsyncThunk("timesheet/fetchOne", () => {
+      throw new UnauthorizedEntityRequestException("timesheets", id);
+    })
+  );
+
+  await act(async () => {
+    render(
+      <Fixture
+        authContextValue={{
+          isAuthenticated: true,
+          user,
+        }}
+        store={store}
+        initialEntries={[`/timesheet/${id}`]}
+      />
+    );
+  });
+
+  expect(timesheetActions.fetchOne).toHaveBeenCalledWith(id);
+  screen.getByTestId("not-found-message");
 });
--- src/timesheets/pages/TimesheetViewPage.tsx
+++ src/timesheets/pages/TimesheetViewPage.tsx
@@ -1,39 +1,119 @@
+import React, { useMemo, useState, useEffect } from "react";
 import DefaultLayout from "common/layouts/DefaultLayout";
 import Messages from "messages/Messages";
 import PageTitle from "common/layouts/PageTitle";
 import TimesheetView from "timesheets/TimesheetView";
-import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
-import { selectTimesheets } from "timesheets/store/timesheets";
+import {
+  selectTimesheets,
+  actions as timesheetActions,
+  UndefinedEntryException,
+} from "timesheets/store/timesheets";
+import { selectShifts, actions as shiftActions } from "timesheets/store/shifts";
+import {
+  selectAbsences,
+  actions as absenceActions,
+} from "timesheets/store/absences";
 import NotFoundPage from "navigation/pages/NotFoundPage";
-import { selectShifts } from "timesheets/store/shifts";
+import { Absence, Entry, Shift, Timesheet } from "timesheets/types";
+import { useThunkDispatch } from "store/createStore";
+import { EntityState } from "store/types";
+import {
+  isMissingAbsences,
+  isMissingShifts,
+  getTimesheetEntries,
+} from "timesheets/helpers";
+import LoadingPage from "common/pages/LoadingPage";
+import { entityStateIsIdle } from "store/entity";
+
+const useTimesheet = (id: string) => {
+  const dispatch = useThunkDispatch();
+  const timesheetsState = useSelector(selectTimesheets);
+  const shiftsState = useSelector(selectShifts);
+  const absencesState = useSelector(selectAbsences);
+  const timesheet = useMemo(() => {
+    const timesheet = timesheetsState.entities.byID[id];
+    return timesheet !== undefined ? timesheet : null;
+  }, [timesheetsState]);
+
+  useEffect(() => {
+    if (entityStateIsIdle(timesheetsState)) {
+      if (timesheetsState.entities.byID[id] === undefined) {
+        (async () => {
+          await dispatch(timesheetActions.fetchOne(id));
+        })();
+      }
+    }
+  }, [timesheetsState]);
+
+  const entries = useMemo(() => {
+    if (timesheet !== null) {
+      const isMissingEntries =
+        isMissingShifts(timesheet, shiftsState) ||
+        isMissingAbsences(timesheet, absencesState);
+      if (!isMissingEntries) {
+        return getTimesheetEntries(timesheet, shiftsState, absencesState);
+      }
+    }
+    return null;
+  }, [timesheet, shiftsState, absencesState]);
+
+  useEffect(() => {
+    if (entityStateIsIdle(shiftsState)) {
+      if (timesheet !== null && isMissingShifts(timesheet, shiftsState)) {
+        (async () => {
+          await dispatch(shiftActions.fetchAllBelongingTo(timesheet.id));
+        })();
+      }
+    }
+  }, [timesheet, shiftsState]);
+
+  useEffect(() => {
+    if (entityStateIsIdle(absencesState)) {
+      if (timesheet !== null && isMissingAbsences(timesheet, absencesState)) {
+        (async () => {
+          await dispatch(absenceActions.fetchAllBelongingTo(timesheet.id));
+        })();
+      }
+    }
+  }, [timesheet, absencesState]);
+
+  return {
+    timesheet,
+    entries,
+    error: timesheetsState.error,
+  };
+};
 
 const TimesheetViewPage: React.FC = () => {
+  const dispatch = useThunkDispatch();
   const { id } = useParams<{
     id: string;
   }>();
-  const { entities: timesheets } = useSelector(selectTimesheets);
-  const { entities: shiftEntities } = useSelector(selectShifts);
-  const timesheet = timesheets.byID[id];
-  const shifts = useMemo(
-    () =>
-      timesheet.relationships.shifts.map(
-        (shiftID) => shiftEntities.byID[shiftID]
-      ),
-    [timesheet, shiftEntities]
-  );
+  const { timesheet, entries, error } = useTimesheet(id);
 
-  return timesheet ? (
+  if (error !== null) {
+    if (error.name === "EntityNotFoundException") {
+      return <NotFoundPage />;
+    }
+    if (error.name === "UnauthorizedEntityRequestException") {
+      return <NotFoundPage />;
+    }
+  }
+
+  if (timesheet === null || entries === null) {
+    return <LoadingPage />;
+  }
+
+  return (
     <DefaultLayout>
       <PageTitle>Timesheet Details</PageTitle>
       <Messages />
       <div className="container">
-        <TimesheetView timesheet={timesheet} entries={shifts} />
+        <TimesheetView timesheet={timesheet} entries={entries} />
       </div>
     </DefaultLayout>
-  ) : (
-    <NotFoundPage />
   );
 };
 
